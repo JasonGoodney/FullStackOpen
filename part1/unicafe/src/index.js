c305c970b214fb961137fca84b4528f46af5c07b
Jason Goodney <jason.goodney@gmail.com>
2019-08-27 16:09:25 -0400

    completed unicafe exercise step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Title = ({ text }) => (
+  <div><h1>{text}</h1></div>
+);
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+const StatisticLabel = ({ text, counter }) => <p>{text} {counter}</p>;
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Title text='give feedback' />
+      <Button text='good' onClick={handleGoodClick} />
+      <Button text='neutral' onClick={handleNeutralClick} />
+      <Button text='bad' onClick={handleBadClick} />
+      <Title text='statistics' />
+      <StatisticLabel text='good' counter={good} />
+      <StatisticLabel text='neutral' counter={neutral} />
+      <StatisticLabel text='bad' counter={bad} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
