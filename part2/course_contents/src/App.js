2abcfbf88b66532cf067a290929d8771f03864e4
Jason Goodney <jason.goodney@gmail.com>
2019-08-29 11:25:25 -0400

    completed part2 course content step6

--- part2/course_contents/src/App.js
+++ part2/course_contents/src/App.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
-const App = () => {
+const Course = ({ course }) => {
   return (
     <div>
-      <h1>hello</h1>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Content = ({ parts }) => {
+  return (
+    <ul>
+      {parts.map((part) =>
+        <Part key={part.id}
+          part={part}
+        />
+      )}
+    </ul>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course} />
     </div>
   );
 };
