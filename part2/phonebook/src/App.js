f4ce2c05f8c6264571da32b5fc26ad727f440df4
Jason Goodney <jason.goodney@gmail.com>
2019-08-29 21:08:46 -0400

    completed part2 phonebook exercies step1 and step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,44 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState('');
 
+  const addName = (event) => {
+    event.preventDefault();
+
+    const hasName = persons
+      .map((person) => person.name)
+      .includes(newName);
+
+    if (hasName) {
+      window.alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personObject = {
+      name: newName
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+
+  };
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
     </div>
   );
 };
