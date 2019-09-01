4e7a2a884134519319332d504ae5d9b8ac24d2d2
Jason Goodney <jason.goodney@gmail.com>
2019-09-01 18:51:47 -0400

    completed part2 countries exercise step3

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Country = ({ country }) => {
   const { name, capital, population, languages, flag } = country;
+  const [currentWeather, setCurrentWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://api-cdn.apixu.com/v1/current.json?key=a784acade42443bc889154200190109&q=${name}`)
+      .then(response => {
+        setCurrentWeather(response.data.current);
+      });
+  }, []);
+
+  const displayWeather = () => {
+    const {
+      condition,
+      wind_kph: windKph,
+      wind_dir: windDirection,
+      temp_c: tempCelsius
+    } = currentWeather;
+
+    return (
+      <div>
+        <h3>Weather in {capital}</h3>
+        <div>
+          <h4 style={{ display: 'inline' }}>
+            temperature:
+          </h4>
+          <p style={{ display: 'inline' }}> {tempCelsius} Celsius</p>
+        </div>
+        <img src={condition && iconSource(condition)} alt='icon' />
+
+        <div>
+          <h4 style={{ display: 'inline' }}>
+            wind:
+          </h4>
+          <p style={{ display: 'inline' }}> {windKph} kph direction {windDirection}</p>
+        </div>
+      </div>
+    );
+  };
+
+  const iconSource = (condition) => `https:${condition.icon}`;
 
   return (
     <div>
@@ -18,7 +59,8 @@ const Country = ({ country }) => {
           )
         }
       </ul>
-      <img src={flag} alt='flag' width={100} height={100} />
+      <img src={flag} alt='flag' height={100} />
+      {currentWeather && displayWeather()}
     </div>
   );
 };
